a4142da8e745b72040e78e0cca5ffdd01d3c96ed
Akshita <akshitagoyal781@gmail.com>
2023-06-03 22:52:27 +0530

    cart waali screen part 2

--- lib/pages/cart/CartPage.dart
+++ lib/pages/cart/CartPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
 
@@ -55,7 +56,7 @@ class _CartPageState extends State<CartPage> {
                 ),
               ),
                   Padding(
-                    padding:  EdgeInsets.symmetric(horizontal: 0.07778*width),
+                    padding:  EdgeInsets.symmetric(horizontal: 0.06*width),
                     child: Card(
                         elevation: 5 ,
                         shadowColor: Colors.black,
@@ -81,33 +82,81 @@ class _CartPageState extends State<CartPage> {
                             ),
                             SizedBox(
                               width:0.88889* width,
-                              child: const Row(
+                              child: Row(
                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Stack(
-                                    alignment: AlignmentDirectional.center,
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0.02*width,0,0,0),
+                                    child: const Stack(
+                                      alignment: AlignmentDirectional.center,
+                                      children: [
+                                        Icon(Icons.crop_square_sharp, color: Colors.green, size: 30,),
+                                        Icon(Icons.circle, color: Colors.green, size: 10),
+                                      ],
+                                    ),
+                                  ),
+                                   Column(
                                     children: [
-                                      Icon(Icons.crop_square_sharp, color: Colors.green, size: 30,),
-                                      Icon(Icons.circle, color: Colors.green, size: 10),
+                                       Padding(
+                                        padding:  EdgeInsets.fromLTRB(0,0.0075*height,0,0),
+                                        child: const Text("Shillong Shezwan Maggi" , style: TextStyle(
+                                          fontFamily: "inter",
+                                          fontWeight: FontWeight.w200
+                                        ),
+                                        ),
+                                      ),
                                     ],
                                   ),
-                                  Text("Shillong Shezwan Maggi" , style: TextStyle(
-                                    fontFamily: "inter",
-                                    fontWeight: FontWeight.w200
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(0,0,0.05*width,0),
+                                    child: const Row(
+                                      children: [
+                                        Icon(Icons.currency_rupee),
+                                        Text("100"),
+                                      ],
+                                    ),
                                   ),
-                                  ),
-
                                 ],
                               ),
-                            )
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0.12222*width , 0 ,0,0),
+                              child: Card(
+                                  elevation: 2 ,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      side: const BorderSide(
+                                        color: Color(0xFFE4BE74),
+                                      )
+                                  ),
+                                  child:  SizedBox(
+                                    height: 0.0325*height,
+                                    width: 0.2055*width,
+                                    child: const Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Icon(Icons.remove , size: 20),
+                                        Padding(
+                                          padding: EdgeInsets.fromLTRB(3,0,3,0),
+                                          child: Text(
+                                            '2',
+                                            style: TextStyle(fontSize: 12 , fontFamily: "inter"),
+                                          ),
+                                        ),
+                                        Icon(Icons.add , size: 20)
 
+                                      ],
+                                    ),
+                                  )
+                              ),
+                            ),
                           ],
                         )
-
                     ),
                   ),
             ],
-          )),
+          )
+          ),
         ],
       ),
     );
