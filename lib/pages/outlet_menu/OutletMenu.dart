84aa8745a20bab724c81033fe4efaed102bf4010
Sanyam Ratreja <sanyamratreja18@gmail.com>
2023-06-08 13:57:58 +0530

    menu screen completed

--- lib/pages/outlet_menu/OutletMenu.dart
+++ lib/pages/outlet_menu/OutletMenu.dart
@@ -370,164 +370,178 @@ class CategoryMenu extends StatelessWidget {
     bool check = (list.length > 1);
     for (var i in list) {
       if (check && i.category != "All") {
-        children.add(Row(
-          children: [
-            Text(
-              i.category,
-              style: const TextStyle(fontSize: 15),
-            ),
-          ],
-        ));
-
-        for (var j in i.products) {
-          children.add(
-            SizedBox(
-              width: 0.8888 * width,
-              height: 0.1575 * height,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: SizedBox(
-                    width: 0.8888 * width,
-                    height: 0.1575 * height,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: 0.2861*width,
-                            height: 0.1275*height,
-                            child: Stack(
-                              children: [
-                                Align(
-                                  alignment: Alignment.topCenter,
+        children.add(
+          Row(
+            children: [
+              Text(
+                i.category,
+                style: const TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+        );
+      }
+      for (var j in i.products) {
+        children.add(
+          SizedBox(
+            width: 0.8888 * width,
+            height: 0.1575 * height,
+            child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: SizedBox(
+                  width: 0.8888 * width,
+                  height: 0.1575 * height,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 12),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 0.2861 * width,
+                          height: 0.1275 * height,
+                          child: Stack(
+                            children: [
+                              Align(
+                                alignment: Alignment.topCenter,
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                      BorderRadius.circular(15)),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(15),
+                                    child: SizedBox(
+                                      width: 0.2861 * width,
+                                      height: 0.1025 * height,
+                                      child: (j.productImage != "null")
+                                          ? Image.network(j.productImage)
+                                          : Image.asset(
+                                          "assets/images/pasta.jpeg"),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomCenter,
+                                child: SizedBox(
+                                  width: 0.205556 * width,
                                   child: Card(
                                     shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15)
+                                      side: const BorderSide(
+                                          color: Color(0xFF004932), width: 1),
+                                      borderRadius: BorderRadius.circular(5),
                                     ),
                                     child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(15),
-                                      child: SizedBox(
-                                        width: 0.2861 * width,
-                                        height: 0.1025 * height,
-                                        child: (j.productImage != "null")
-                                            ? Image.network(j.productImage)
-                                            : Image.asset("assets/images/pasta.jpeg"),
+                                      borderRadius: BorderRadius.circular(5),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          SizedBox(
+                                            width: 0.06777 * width,
+                                            height: 0.03125 * height,
+                                            child: ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                  backgroundColor:
+                                                  const Color(0xFFD6EAE1),
+                                                  padding: EdgeInsets.zero),
+                                              onPressed: () {},
+                                              child: const Icon(
+                                                Icons.remove,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ),
+                                          const Text("1"),
+                                          SizedBox(
+                                            width: 0.06777 * width,
+                                            height: 0.03125 * height,
+                                            child: ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                  backgroundColor:
+                                                  const Color(0xFFD6EAE1),
+                                                  padding: EdgeInsets.zero),
+                                              onPressed: () {},
+                                              child: const Icon(
+                                                Icons.add,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
                                   ),
                                 ),
-                                Align(
-                                  alignment: Alignment.bottomCenter,
-                                  child: SizedBox(
-                                    width: 0.205556*width,
-                                    child: Card(
-                                      shape: RoundedRectangleBorder(
-                                        side: const BorderSide(
-                                          color: Color(0xFF004932),
-                                          width: 1
-                                        ),
-                                        borderRadius: BorderRadius.circular(5),
-                                      ),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(5),
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            SizedBox(
-                                              width: 0.06777*width,
-                                              height: 0.03125*height,
-                                              child: ElevatedButton(
-                                                style: ElevatedButton.styleFrom(
-                                                  backgroundColor: const Color(0xFFD6EAE1),
-                                                  padding: EdgeInsets.zero
-                                                ),
-                                                onPressed: () {},
-                                                child: const Icon(Icons.remove, color: Colors.black,),
-                                              ),
-                                            ),
-                                            const Text("1"),
-                                            SizedBox(
-                                              width: 0.06777*width,
-                                              height: 0.03125*height,
-                                              child: ElevatedButton(
-                                                style: ElevatedButton.styleFrom(
-                                                    backgroundColor: const Color(0xFFD6EAE1),
-                                                    padding: EdgeInsets.zero
-                                                ),
-                                                onPressed: () {},
-                                                child: const Icon(Icons.add, color: Colors.black,),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                          EdgeInsets.fromLTRB(0.0611 * width, 0, 0, 0),
+                          child: SizedBox(
+                            width: 0.456 * width,
+                            height: 0.08625 * height,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    VegetarianSymbol(
+                                        color: j.veg
+                                            ? Colors.green
+                                            : Colors.red),
+                                    SizedBox(
+                                      width: 0.38 * width,
+                                      child: Text(
+                                        j.name,
+                                        overflow: TextOverflow.ellipsis,
                                       ),
                                     ),
-                                  ),
+                                  ],
                                 ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.fromLTRB(
+                                          0.0566 * width, 0, 0, 0),
+                                      width: 0.4 * width,
+                                      child: Text(
+                                        "₹${j.price}",
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      width: 0.4 * width,
+                                      padding: EdgeInsets.fromLTRB(
+                                          0.0566 * width, 0, 0, 0),
+                                      child: Text(
+                                        j.description,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ],
+                                )
                               ],
                             ),
                           ),
-                          Padding(
-                            padding: EdgeInsets.fromLTRB(0.0611*width, 0,0,0),
-                            child: SizedBox(
-                              width: 0.456*width,
-                              height: 0.08625*height,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    children: [
-                                      VegetarianSymbol(color: j.veg?Colors.green:Colors.red),
-                                      SizedBox(
-                                        width: 0.38*width,
-                                        child: Text(
-                                            j.name,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Container(
-                                        padding: EdgeInsets.fromLTRB(0.0566*width,0,0,0),
-                                        width: 0.4*width,
-                                        child: Text(
-                                            "₹${j.price}",
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      Container(
-                                        width: 0.4*width,
-                                        padding: EdgeInsets.fromLTRB(0.0566*width,0,0,0),
-                                        child: Text(
-                                          j.description,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
+                        )
+                      ],
                     ),
                   ),
                 ),
               ),
             ),
-          );
-        }
+          ),
+        );
       }
     }
 
